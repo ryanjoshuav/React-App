4915ee405b37612e1866fb39a77d1c1b9b1ad961
ryanjoshuav <ryanjoshuavillanueva@gmail.com>
2023-11-28 05:42:50 +0800

    last

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
       <nav>
         <ul>
           <li>
-            <Link to={"/"}>Home</Link>
+            <Link to={"/React-App/"}>Home</Link>
           </li>
           <li>
             <Link to={"post"}>Post</Link>
